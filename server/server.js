ff4269793ed3e8ac01301ac76f94aa2a947551e5
tyudosen@gmail.com <tyudosen@gmail.com>
2020-06-17 19:10:09 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const port = process.env.PORT || 3001
+
+const publicPath = path.join(__dirname, '..client/build')
+
+
+const app = express()
+
+app.use(express.static(publicPath))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+
+app.listen( port, () => {
+    console.log('Running on port 3001');
+    
+})
