46ad7d2dfd5b6ca37e2ce612282612cfff97896d
tyudosen@gmail.com <tyudosen@gmail.com>
2020-06-21 14:30:00 -0500

    projects.js

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -7,7 +7,7 @@ const Projects = () => (
     <Grid style={{ marginTop: '30px'}}>
         <Cell col={12}>
             <div className='projects-grid'>
-                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <Card className='project-card' shadow={5} style={{minWidth: '450', margin: 'auto'}}>
                     <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn0.iconfinder.com/data/icons/long-shadow-web-icons/512/nodejs-512.png) center / cover'}}>NodeJs Project 1</CardTitle>
                     <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus nulla nec sapien cursus tempor. Curabitur urna lectus, molestie id malesuada ut, vulputate vel neque. Phasellus pretium neque quis nibh tincidunt, at aliquet risus euismod. Nulla bibendum orci scelerisque enim pulvinar consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin a bibendum erat. Sed venenatis maximus nibh hendrerit accumsan. Nam lobortis dui ac diam dictum mattis. </CardText>
                     <CardActions border>
@@ -18,7 +18,7 @@ const Projects = () => (
                     </CardMenu>
                 </Card>
 
-                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <Card className='project-card' shadow={5} style={{minWidth: '450', margin: 'auto'}}>
                     <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn0.iconfinder.com/data/icons/long-shadow-web-icons/512/nodejs-512.png) center / cover'}}>NodeJs Project 2</CardTitle>
                     <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus nulla nec sapien cursus tempor. Curabitur urna lectus, molestie id malesuada ut, vulputate vel neque. Phasellus pretium neque quis nibh tincidunt, at aliquet risus euismod. Nulla bibendum orci scelerisque enim pulvinar consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin a bibendum erat. Sed venenatis maximus nibh hendrerit accumsan. Nam lobortis dui ac diam dictum mattis. </CardText>
                     <CardActions border>
