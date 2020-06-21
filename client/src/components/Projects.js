c4f6dd84898ad57caae5e00fd9fc07a6b4dae4e2
tyudosen@gmail.com <tyudosen@gmail.com>
2020-06-21 15:40:07 -0500

    projects.js

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -7,7 +7,8 @@ const Projects = () => (
     <Grid style={{ marginTop: '30px'}}>
         <Cell col={12}>
             <div className='projects-grid'>
-                <Card className='project-card' shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <div className='project-card'>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
                     <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn0.iconfinder.com/data/icons/long-shadow-web-icons/512/nodejs-512.png) center / cover'}}>NodeJs Project 1</CardTitle>
                     <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus nulla nec sapien cursus tempor. Curabitur urna lectus, molestie id malesuada ut, vulputate vel neque. Phasellus pretium neque quis nibh tincidunt, at aliquet risus euismod. Nulla bibendum orci scelerisque enim pulvinar consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin a bibendum erat. Sed venenatis maximus nibh hendrerit accumsan. Nam lobortis dui ac diam dictum mattis. </CardText>
                     <CardActions border>
@@ -17,7 +18,9 @@ const Projects = () => (
                     <CardMenu style={{color: '#fff'}}>
                     </CardMenu>
                 </Card>
-
+                </div>
+                
+                <div className='project-card'>
                 <Card className='project-card' shadow={5} style={{minWidth: '450', margin: 'auto'}}>
                     <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn0.iconfinder.com/data/icons/long-shadow-web-icons/512/nodejs-512.png) center / cover'}}>NodeJs Project 2</CardTitle>
                     <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus nulla nec sapien cursus tempor. Curabitur urna lectus, molestie id malesuada ut, vulputate vel neque. Phasellus pretium neque quis nibh tincidunt, at aliquet risus euismod. Nulla bibendum orci scelerisque enim pulvinar consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin a bibendum erat. Sed venenatis maximus nibh hendrerit accumsan. Nam lobortis dui ac diam dictum mattis. </CardText>
@@ -28,6 +31,59 @@ const Projects = () => (
                     <CardMenu style={{color: '#fff'}}>
                     </CardMenu>
                 </Card>
+                </div>
+
+                <div className='project-card'>
+                <Card className='project-card' shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn0.iconfinder.com/data/icons/long-shadow-web-icons/512/nodejs-512.png) center / cover'}}>NodeJs Project 2</CardTitle>
+                    <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus nulla nec sapien cursus tempor. Curabitur urna lectus, molestie id malesuada ut, vulputate vel neque. Phasellus pretium neque quis nibh tincidunt, at aliquet risus euismod. Nulla bibendum orci scelerisque enim pulvinar consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin a bibendum erat. Sed venenatis maximus nibh hendrerit accumsan. Nam lobortis dui ac diam dictum mattis. </CardText>
+                    <CardActions border>
+                        <Button colored ripple>Github</Button>
+                        <Button colored ripple>Live Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                    </CardMenu>
+                </Card>
+                </div>
+
+                <div className='project-card'>
+                <Card className='project-card' shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn0.iconfinder.com/data/icons/long-shadow-web-icons/512/nodejs-512.png) center / cover'}}>NodeJs Project 2</CardTitle>
+                    <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus nulla nec sapien cursus tempor. Curabitur urna lectus, molestie id malesuada ut, vulputate vel neque. Phasellus pretium neque quis nibh tincidunt, at aliquet risus euismod. Nulla bibendum orci scelerisque enim pulvinar consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin a bibendum erat. Sed venenatis maximus nibh hendrerit accumsan. Nam lobortis dui ac diam dictum mattis. </CardText>
+                    <CardActions border>
+                        <Button colored ripple>Github</Button>
+                        <Button colored ripple>Live Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                    </CardMenu>
+                </Card>
+                </div>
+
+                <div className='project-card'>
+                <Card className='project-card' shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn0.iconfinder.com/data/icons/long-shadow-web-icons/512/nodejs-512.png) center / cover'}}>NodeJs Project 2</CardTitle>
+                    <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus nulla nec sapien cursus tempor. Curabitur urna lectus, molestie id malesuada ut, vulputate vel neque. Phasellus pretium neque quis nibh tincidunt, at aliquet risus euismod. Nulla bibendum orci scelerisque enim pulvinar consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin a bibendum erat. Sed venenatis maximus nibh hendrerit accumsan. Nam lobortis dui ac diam dictum mattis. </CardText>
+                    <CardActions border>
+                        <Button colored ripple>Github</Button>
+                        <Button colored ripple>Live Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                    </CardMenu>
+                </Card>
+                </div>
+
+                <div className='project-card'>
+                <Card className='project-card' shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn0.iconfinder.com/data/icons/long-shadow-web-icons/512/nodejs-512.png) center / cover'}}>NodeJs Project 2</CardTitle>
+                    <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus nulla nec sapien cursus tempor. Curabitur urna lectus, molestie id malesuada ut, vulputate vel neque. Phasellus pretium neque quis nibh tincidunt, at aliquet risus euismod. Nulla bibendum orci scelerisque enim pulvinar consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin a bibendum erat. Sed venenatis maximus nibh hendrerit accumsan. Nam lobortis dui ac diam dictum mattis. </CardText>
+                    <CardActions border>
+                        <Button colored ripple>Github</Button>
+                        <Button colored ripple>Live Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                    </CardMenu>
+                </Card>
+                </div>
             </div>
         </Cell>
     </Grid>
