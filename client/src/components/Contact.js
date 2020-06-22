04aa88fd56bc18ffcf4d8fa70d782e6320ccf6bd
tyudosen@gmail.com <tyudosen@gmail.com>
2020-06-22 15:26:42 -0500

    contact.js

---
+++ client/src/components/Contact.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { List, ListItem, ListItemContent } from 'react-mdl'
+import '../App.css'
+
+const Contact = () => (
+    <div className='contact-container'>
+        <div className='contact-item'>
+            <h2>Toyoabasi Udosen</h2>
+            <img
+                className='avatar'
+                src='/img/avatar.png'
+                alt='avatar'
+            />
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum tristique justo, eget consequat nulla sagittis eget. Curabitur quis sollicitudin magna, a euismod sem. Fusce mollis placerat metus, sed volutpat ipsum lobortis eget. Vestibulum ac nisl neque. Maecenas a molestie leo, a lacinia nisl. Proin dolor augue, rhoncus in molestie eget, convallis in elit. Ut sem ante, cursus molestie mollis vitae, tempor a ex. Pellentesque eu porta enim. Mauris eros purus, gravida vel lacinia in, vehicula sed leo. In sed dui nunc. Aenean vitae magna magna. Etiam maximus pulvinar felis a vulputate. 
+            </p>
+        </div>
+        <div className='contact-item'>
+            <h2>Contact Me</h2>
+            <hr/>
+            <div className='contact-list'>
+                <List >
+                    <ListItem>
+                        <ListItemContent>
+                            <i class="fa fa-phone-square" aria-hidden="true"> 1 (346) 252-2540</i>
+                        </ListItemContent>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemContent>
+                            <i class="fa fa-envelope" aria-hidden="true"> toyo.i.udosen@gmail.com</i>
+                        </ListItemContent>
+                    </ListItem>
+                </List>
+            </div>
+        </div>
+    </div>
+)
+
+export default Contact;
--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const Resume = () => (
-    <h1>Resume</h1>
+    <div>
+    </div>
 )
 
-export default Resume;
+export default Resume
