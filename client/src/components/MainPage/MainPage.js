19d8be1b268453c97151e7d63746185e93b61ce5
tyudosen@gmail.com <tyudosen@gmail.com>
2020-06-23 23:24:00 -0500

    mainPage.js

--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -1,36 +1,41 @@
-import React from 'react'
-import './MainPage.css'
-import {Grid, Cell} from 'react-mdl'
+import React from "react";
+import "./MainPage.css";
+import Footer from "../Footer/Footer";
+import { Grid, Cell } from "react-mdl";
 
+const MainPage = () => (
+  <div>
+    <div className="banner">
+      <div className="text-banner">
+        <h2>Full Stack Developer</h2>
+        <hr />
+        <p>JavaScript | NodeJs | React | HTML/CSS | Express | MongoDB</p>
 
-const MainPage = () =>(
-    <div className='banner'>
-        <div className='text-banner'>
-            <h2>Full Stack Developer</h2>
-            <hr/>
-            <p>
-                JavaScript | NodeJs | React | HTML/CSS | Express | MongoDB
-            </p>
+        <div className="social-links">
+          <a
+            href="http://linkedin.com/in/toyoudosen"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-linkedin-square" aria-hidden="true" />
+          </a>
 
-            <div className='social-links'>
-                        <a href='http://linkedin.com/in/toyoudosen' target='_blank' rel='noopener noreferrer'>
-                            <i className='fa fa-linkedin-square' aria-hidden='true' />
-                        </a>
-
-                        <a href='https://github.com/tyudosen?tab=repositories' target='_blank' rel='noopener noreferrer'>
-                            <i class="fa fa-github-square" aria-hidden="true"></i>
-                        </a>
-                    </div>
-            </div>
-            
-        <div className='img'>
-            <img
-                className='avatar-one'
-                src='/img/avatar.png'
-                alt='avatar'
-            />
+          <a
+            href="https://github.com/tyudosen?tab=repositories"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fa fa-github-square" aria-hidden="true"></i>
+          </a>
         </div>
+      </div>
+
+      <div className="img">
+        <img className="avatar-one" src="/img/avatar.png" alt="avatar" />
+      </div>
     </div>
-)
+    <Footer />
+  </div>
+);
 
 export default MainPage;
